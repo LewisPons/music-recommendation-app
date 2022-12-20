3f5734e5722c1521a0b4c7ef345c8fe53a91f080
LewisPons <morales_ponce@hotmail.com>
2022-12-20 15:51:39 -0600

    Build app v.0.1

---
+++ app.py
@@ -0,0 +1,107 @@
+
+import streamlit as st
+import pandas as pd
+from PIL import Image
+from tqdm import tqdm
+from utils import call_playlist, music_recomendation, get_songs_visuals, SONG_FEATURES, visualize_cover_art
+import codecs
+
+# https://open.spotify.com/playlist/7sY3tKQa40BXziDEer3m3I?si=89f0cffef02e41ec
+
+st.set_page_config(page_title="Music Recomendation App")
+
+# CLIENT_ID="da82a340bd1341599bd3c590de6ad9fa", 
+# CLIENT_SECRET="2c8640b95bc14b3b8edeab8585bf13af"
+
+CLIENT_ID = st.secrets('client_id')
+CLIENT_SECRET = st.secrets('client_secret')
+
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+image = Image.open('spotify_logo.png')
+
+st.sidebar.image(image , caption="Spotify Recomendation System",width = 256)
+app_mode = st.sidebar.selectbox("Choose app mode", ["Run App", "Obtain your Spotify URI","About Me"])
+
+@st.cache
+def read_full_dataset(path: str = 'popular_music.parquet.gzip') -> pd.DataFrame:
+    return pd.read_parquet(path)
+
+
+
+if app_mode == 'Run App':
+    st.title('Music Recomendation System')
+    markdown = """
+    This is a first-version Content Based Music Recommendation System.\n
+    The model was trained based on this [dataset](https://www.kaggle.com/datasets/yamaerenay/spotify-dataset-19212020-600k-tracks).\n
+    \n
+    To start generating recomendations:
+    1. Open Spotify.
+    2. Select your favourite Playlist.
+    3. Copy the playlist URL (It should be something like this (https://open.spotify.com/playlist/7sY3tKQa40BXziDEer3m3I?si=89f0cffef02e41ec).
+    4. Paste the URL into the text box.
+    5. Select the song you want recomendations for.
+    6. Have fun!
+    """
+    
+    st.markdown(markdown)
+
+    playlist_link = st.text_input('**Input your playlist URI**')
+    
+    
+    dataset = read_full_dataset()
+    
+    if (len(playlist_link) != 0) & (playlist_link[:34] == 'https://open.spotify.com/playlist/'):
+        df = call_playlist(playlist_link, CLIENT_ID, CLIENT_SECRET)
+        
+        st.dataframe(df)
+        # data = st.cache(pd.read_csv)
+        selected_indices = st.multiselect('Select a song to generate recomendations:', df.index)
+        
+        if len(selected_indices) == 1:
+            selected_song_df = df.loc[selected_indices]
+            st.write('Selected Song:', selected_song_df)
+            recomendations_df = music_recomendation(dataset, selected_song_df)
+            recomendations_df = get_songs_visuals(recomendations_df, CLIENT_ID, CLIENT_SECRET)
+            
+            
+            st.subheader('Recomendations generated:')
+            st.write('Check out the top 3 songs for you!', recomendations_df) 
+            
+            st.markdown(f"![Alt Text]({recomendations_df['url'][0]})")
+            st.markdown(f"![Alt Text]({recomendations_df['url'][1]})")
+            st.markdown(f"![Alt Text]({recomendations_df['url'][2]})")
+            
+
+            
+            
+        else:
+            st.write('Please Select only one song')
+    else:
+        st.write('Please check the Playlist link is correct :)')
+        
+elif app_mode == "About Me":
+    st.title('Music Recomendation System')
+    mkdn = """
+    Hi! My name is Luis Morales, I am a Data Engineer at Wizeline and B.Sc in Actuarial Sciences. \n
+    I love working in Cloud and feel passionated about Machine Learning and AI!.
+    """
+    st.markdown(mkdn)
+    st.success("Feel free to contacting me here 👇 ")
+
+    col1,col2,col3,col4 = st.columns((2,1,2,1))
+    col1.markdown('* [**LinkedIn**](https://www.linkedin.com/in/luis-morales-ponce/)')
+    col1.markdown('* [**GitHub**](https://github.com/LewisPons)')
+    image2 = Image.open('profile.jpeg')
+    st.image(image2,width=230)
+
+elif app_mode =="Obtain your Spotify URI":
+    stepflow_file = codecs.open("SpotifyWorkflow.html", "r", "utf-8")
+    st.markdown(stepflow_file.read(), unsafe_allow_html=True)
+
---
+++ utils.py
@@ -0,0 +1,127 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from credentials import CLIENT_ID, CLIENT_SECRET
+
+import pandas as pd
+import numpy as np 
+from skimage import io
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+SONG_FEATURES = ['danceability', 'energy', 'key',
+       'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness',
+       'liveness', 'valence', 'tempo', 'time_signature']
+
+
+def spotipy_authorize(cid: str = CLIENT_ID, secret: str = CLIENT_SECRET):
+    """
+        Get Spotify API Credentials manager
+    """
+    sp = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=cid, 
+            client_secret=secret
+        )
+    )
+    return sp
+
+
+def call_playlist(playlist_uri : str, cid : str, client_secret : str, creator='spotify') -> pd.DataFrame:
+    sp = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=cid, 
+            client_secret=client_secret
+        )
+    )
+
+    playlist_features_list = ['artist', 'album', 'track_name', 'track_id','url', 'artist_popularity', 'artist_genre', 'danceability',
+                                'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness','instrumentalness', 'liveness', 
+                                'valence', 'tempo', 'duration_ms', 'time_signature']
+    
+    playlist_df = pd.DataFrame(columns = playlist_features_list)
+    artist_df = pd.DataFrame(columns=['artist_popularity', 'artist_genre'])
+    
+    playlist = sp.user_playlist_tracks(creator, playlist_uri)["items"]
+    for track in playlist:
+        playlist_features = {}
+        artist_features = {}
+        # Get metadata
+        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        playlist_features["album"] = track["track"]["album"]["name"]
+        playlist_features["track_name"] = track["track"]["name"]
+        playlist_features["track_id"] = track["track"]["id"]
+        playlist_features["url"] = track['track']['album']['images'][1]['url']
+        
+        artist_uri = track["track"]["artists"][0]["uri"]
+        artist_info = sp.artist(artist_uri)
+        
+        
+        artist_features["artist_popularity"] = artist_info["popularity"]
+        artist_features["artist_genre"] = str(artist_info["genres"])
+        popularity = (artist_info["popularity"])
+        genres = str(artist_info["genres"])
+        # Get audio features
+        audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for feature in playlist_features_list[7:]:
+            playlist_features[feature] = audio_features[feature]
+            
+        # Concat the 
+        artist_df = pd.DataFrame(artist_features, index=[0])
+        track_df = pd.DataFrame(playlist_features, index = [0])
+        
+        artist_track_df = pd.concat([track_df, artist_df], axis=1)
+        
+        playlist_df = pd.concat([playlist_df, artist_track_df], ignore_index = True)
+            
+    return playlist_df
+
+
+def music_recomendation(dataset: pd.DataFrame, song_df: pd.DataFrame):
+    numerical_features_dataset = dataset[SONG_FEATURES]
+    numerical_features_song = song_df[SONG_FEATURES]
+    
+    similarity = cosine_similarity(numerical_features_dataset, numerical_features_song)
+    similarity_df = pd.DataFrame(similarity, columns=['cosine_similarity'])
+    
+    results = pd.concat([dataset, similarity_df], axis=1)
+    
+    return results.sort_values('cosine_similarity', ascending=False).reset_index(drop=True).head()
+
+
+def get_songs_visuals(df: pd.DataFrame, cid: str, client_secret: str) -> pd.DataFrame:
+    sp = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=cid, 
+            client_secret=client_secret
+        )
+    )
+    ids = df['id'].to_list()
+    urls = []
+    for id in ids:
+        urn = f'spotify:track:{id}'
+        track_url = sp.track(urn)['album']['images'][0]['url']
+        urls.append(track_url)
+    
+    df['url'] = urls
+    return df
+
+
+def visualize_cover_art(playlist_df):
+    temp = playlist_df['url'].values
+    plt.figure(figsize=(15,int(0.625 * len(temp))))
+    columns = 5
+    
+    for i, url in enumerate(temp):
+        plt.subplot(int(len(temp) / columns + 1), columns, i + 1)
+
+        image = io.imread(url)
+        plt.imshow(image)
+        plt.xticks([])
+        plt.yticks([])
+        s='' 
+        plt.xlabel(s.join(playlist_df['track_name'].values[i].split(' ')[:4]), fontsize = 10, fontweight='bold')
+        plt.tight_layout(h_pad=0.8, w_pad=0)
+        plt.subplots_adjust(wspace=None, hspace=None)
+    
+    plt.savefig('recommended_songs.png', dpi=400)
